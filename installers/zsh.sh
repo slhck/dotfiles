020747130b372472d2bd8c0fd6be70641bb87efc
Werner Robitza <werner.robitza@gmail.com>
2019-03-20 18:45:28 +0100

    fix zsh home

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # ZSH-relevant things
 
-zshRoot="/home/$USER/.oh-my-zsh/custom"
+zshRoot="$HOME/.oh-my-zsh/custom"
 
 # Spaceship theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$zshRoot/themes/spaceship-prompt"
 ln -s "$zshRoot/themes/spaceship-prompt/spaceship.zsh-theme" "$zshRoot/themes/spaceship.zsh-theme"
 perl -pi -e 's/blinks/spaceship/' ~/.zshrc
+
