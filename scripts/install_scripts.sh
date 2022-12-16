a764d7cddb752ca087ef521073884fd208b78fe8
Werner Robitza <werner.robitza@gmail.com>
2022-12-16 17:18:14 +0100

    update installer script

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -4,4 +4,13 @@ cd "$(dirname "$0")" || exit 1
 
 mkdir -p "$HOME/.bin"
 
-cp -v -- release-python.sh "$HOME/.bin"
+for script in *; do
+    # skip if the script is this script
+    if [[ "$script" == "install_scripts.sh" ]]; then
+        continue
+    fi
+
+    if [[ -f "$script" && -x "$script" ]]; then
+        cp -v -- "$script" "$HOME/.bin"
+    fi
+done
