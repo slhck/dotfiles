40bcf8cb53a4d82938b7bf16adca8dcd88724802
Werner Robitza <werner.robitza@gmail.com>
2023-03-25 16:52:26 +0100

    update

--- installers/node.sh
+++ installers/node.sh
@@ -3,15 +3,9 @@
 nvm install node && \
     npm install -g \
     auto-changelog \
-    eslint \
-    grunt \
-    js-beautify \
-    jshint \
-    jslint \
-    jsonlint \
     npm-check-updates \
     prettier \
     state-machine-cat \
     tldr \
-    web-ext \
-    yarn
+    yarn \
+    @githubnext/github-copilot-cli
