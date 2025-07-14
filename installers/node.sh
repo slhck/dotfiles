e95e126f9ec01ad47f39e47a1ea3918304a4ae50
Werner Robitza <werner.robitza@gmail.com>
2025-07-14 14:29:12 +0200

    add some node pacakges

--- installers/node.sh
+++ installers/node.sh
@@ -5,3 +5,17 @@ nvm install --lts
 
 npm install -g \
     yarn
+
+# some other tools that are generally useful
+tools=(
+    @anthropic-ai/claude-code
+    @google/gemini-cli
+    @openai/codex
+)
+
+for tool in "${tools[@]}"; do
+    echo "Installing $tool"
+    npm install -g "$tool"
+done
+
+echo "Done"
