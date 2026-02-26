d085d4586e27fa02cc25df225d276410857e0a23
Werner Robitza <werner.robitza@gmail.com>
2026-02-26 09:31:05 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -59,12 +59,12 @@ SELECTED_COMPONENTS=()
 # Colors and logging
 #------------------------------------------------------------------------------
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-BOLD='\033[1m'
-NC='\033[0m'
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+BOLD=$'\033[1m'
+NC=$'\033[0m'
 
 log()         { echo -e "${BLUE}==>${NC} $1"; }
 log_success() { echo -e "${GREEN}✓${NC} $1"; }
